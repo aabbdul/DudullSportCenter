21ebc170650cbbb40b9da78c401ee5acca8749c0
Abdul Karim Zaidan <zaidan.abdul22@gmail.com>
2024-11-14 17:04:17 +0700

    akucintamatisamapython

--- app.py
+++ app.py
@@ -56,23 +56,6 @@ def auth():
     
     return redirect(url_for('register'))
 
-@app.route('/authAdmin', methods=['GET','POST'])
-def authAdmin():
-    if 'login' in request.form:
-
-        username = request.form['username']
-        password = request.form['password']
-        user = mongo.db.admin.find_one({'username': username})
-
-        if user and check_password_hash(user['password'], password):
-            session['username'] = username
-            flash("You're now admin", 'success')
-            return redirect(url_for('admin'))
-        else:
-            flash('Invalid username or password', 'danger')
-    
-    return redirect(url_for('admin'))
-
 @app.route('/bookingFutsal')
 def bookFutsal():
     return render_template('booking/futsal.html')
@@ -81,9 +64,6 @@ def bookFutsal():
 def bookBadminton():
     return render_template('booking/badminton.html')
 
-@app.route('/admin')
-def admin():
-    return render_template('admin/admin.html')
 
 @app.route('/logout')
 def userLogout():
@@ -91,11 +71,6 @@ def userLogout():
     flash('You have been logged out.', 'success')
     return redirect(url_for('home'))
 
-@app.route('/adminlogout')
-def adminLogout():
-    session.pop('username', None)
-    flash("You're no longer admin.", 'success')
-    return redirect(url_for('admin'))
 
 if __name__ == '__main__':
     app.run(debug=True)
